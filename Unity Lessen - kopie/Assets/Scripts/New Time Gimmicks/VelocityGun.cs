2cb86f5c26b2f0af2468cf83ccd8a481e473b173
LorenzoHKU <lorenzohartman2000@gmail.com>
2019-03-07 13:20:22 +0100

    Working Velocity Gun

--- Unity Lessen - kopie/Assets/Scripts/New Time Gimmicks/VelocityGun.cs
+++ Unity Lessen - kopie/Assets/Scripts/New Time Gimmicks/VelocityGun.cs
@@ -11,8 +11,13 @@ public class VelocityGun : MonoBehaviour
     public float fireRate = 15f;
     private float nextTimeToFire = 0f;
     public Animation anim;
+    public enum gunType {Slowdown, Increase, Stop};
+    public gunType TypeofGun;
+    public float waitTime;
+    IEnumerator coroutine;
 
-    public GameObject otherObject;
+
+
     Animator otherAnimator;
 
 
@@ -26,7 +31,7 @@ public class VelocityGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
 
@@ -34,31 +39,80 @@ public class VelocityGun : MonoBehaviour
         }
 
 
+        if (Input.GetButton("Ability1"))
+        {
+            TypeofGun = gunType.Slowdown; 
+        }
+        if (Input.GetButton("Ability2"))
+        {
+            TypeofGun = gunType.Increase;
+        }
+        if (Input.GetButton("Ability3"))
+        {
+            TypeofGun = gunType.Stop;
+        }
+
 
     }
 
-
-
-    void Shoot()
+void Shoot()
     {
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) && hit.transform.tag == "influenceable")
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) )
         {
+            Transform otherObject = hit.transform;
+            while (otherObject.tag != "influenceable" )
+            {
+                if(otherObject.parent == null) { return; }
+                otherObject = otherObject.parent;
+            }
+
+            if (otherObject.GetComponent<Animator>() == null) return;
+
             //(Debug Message to check if it actually hit.)
-            Debug.Log("Gottem");
+            Debug.Log("Gottem"+otherObject.name);
 
             //Sets otherObject to the hit gameobject. 
-            otherObject = hit.transform.gameObject;
+            
             //Sets OtherAnimator to the Animator component we have just received from the raycast hit. 
             otherAnimator = otherObject.GetComponent<Animator>();
             //Changes the animation speed to a lower value to create the illusion of slowing down. 
-            otherAnimator.speed = 0.1f; 
+
+            switch(TypeofGun)
+            {
+                case gunType.Slowdown:
+                    otherAnimator.speed = 0.1f;
+                    break;
+
+                case gunType.Increase:
+                    otherAnimator.speed = 2f;
+                    break;
+
+                case gunType.Stop:
+                    otherAnimator.speed = 0f;
+                    break; 
+            }
+
+            //Resets animator speed
+            StopCoroutine("ResetSpeed"); 
+
+            coroutine = ResetSpeed(otherAnimator);
+            
+            StartCoroutine(coroutine); 
+
+
         }
+
+
     }
 
     
 
-
+    private IEnumerator ResetSpeed(Animator am)
+    {
+        yield return new WaitForSeconds(waitTime);
+        am.speed = 1f;
+    }
     
 
 
