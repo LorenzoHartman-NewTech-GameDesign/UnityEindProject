37c3e313c965e701ccf3303745cd62606af294f4
LorenzoHKU <lorenzohartman2000@gmail.com>
2019-03-14 11:41:54 +0100

    no message

---
+++ Unity Lessen - kopie/Assets/Animations/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+
+      
+
+
+{
+
+    public GameObject DoorObject;
+    Animator otherAnimator; 
+
+
+    void OpenDoor()
+    {
+        otherAnimator = DoorObject.GetComponent<Animator>();
+        otherAnimator.gameObject.GetComponent<Animator>().enabled = true;
+        otherAnimator.Play("OpenDoor");
+    }
+
+
+    void CloseDoor()
+    {
+        otherAnimator = DoorObject.GetComponent<Animator>();
+        otherAnimator.gameObject.GetComponent<Animator>().enabled = true;
+        otherAnimator.Play("CloseDoor");
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+
+
+
+
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
