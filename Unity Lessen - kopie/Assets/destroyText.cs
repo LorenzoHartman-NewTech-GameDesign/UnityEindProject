81d5f66e53307d7425076947b705e63e9a680281
LorenzoHKU <lorenzohartman2000@gmail.com>
2019-03-29 22:29:16 +0100

    Added Skybox with night sky

---
+++ Unity Lessen - kopie/Assets/destroyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class destroyText : MonoBehaviour
+{
+
+
+
+    public GameObject text; 
+
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(text); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
